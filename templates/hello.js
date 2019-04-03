da9b0afd488820f2b623b4bd40b6765fc4681d04
hemapriyamaheswaran <45891957+hemapriyamaheswaran@users.noreply.github.com>
2019-04-03 18:27:11 +0530

    Update hello.js

--- templates/hello.js
+++ templates/hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Hemapriya\n');
+    response.end('Hello Hemapriyamaheswaran\n');
 }).listen(8080);
 
 console.log('Server started');
